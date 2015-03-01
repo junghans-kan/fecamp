e977a7b28947e298a3edf576f7077032e265d48d
junghans-kan <jourhans@gmail.com>
2015-03-01 22:53:08 +0900

    feb last week

--- html/default.js
+++ html/default.js
@@ -2,5 +2,13 @@ jQuery(function($){
 	$(".gnb>button").on("click", function(){
 		//$(".gnb>ul").slideToggle(200);
 		$(this).next("ul").slideToggle(200);
+
+		if( $(".gnb>button").text() ==="메뉴 열기" ){
+			//메뉴 닫기 텍스트
+			$(".gnb>button").text("메뉴 닫기");
+		} else {
+			//메뉴 열기 텍스트 표시
+			$(".gnb>button").text("메뉴 열기");
+		}
 	});
 });
